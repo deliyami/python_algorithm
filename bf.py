9122555ffd6591a052bf7fa616659934184a94ec
deliyami <dkdidudtn@gmail.com>
2022-05-16 16:47:07 +0900

    ADD - bellman ford algorithm

--- bf.py
+++ bf.py
@@ -1,54 +1,80 @@
-import sys
-
-input = sys.stdin.readline
 INF = int(1e9)
 
-# 노드의 개수, 간선의 개수를 입력
-v, e = map(int, input().split())
-# 모든 간선에 대한 정보를 담는 리스트 만들기
-edges = []
-# 최단 거리 테이블을 모두 무한으로 초기화
-distance = [INF] * (v + 1)
+# 모든 변의 정보 입력
+sides = [
+    ['A', 'B', 9],
+    ['A', 'C', 2],
+    ['B', 'A', 9],
+    ['B', 'C', 6],
+    ['B', 'D', 3],
+    ['B', 'E', 1],
+    ['C', 'A', 2],
+    ['C', 'B', 6],
+    ['C', 'D', 2],
+    ['C', 'F', 9],
+    ['D', 'B', 3],
+    ['D', 'C', 2],
+    ['D', 'E', 5],
+    ['D', 'F', 6],
+    ['E', 'B', 1],
+    ['E', 'D', 5],
+    ['E', 'F', 3],
+    ['E', 'G', 7],
+    ['F', 'C', 9],
+    ['F', 'D', 6],
+    ['F', 'E', 3],
+    ['F', 'G', 4],
+    ['G', 'E', 7],
+    ['G', 'F', 4],
+]
 
-# 모든 간선의 정보 입력
-for _ in range(e):
-    a, b, c = map(int, input().split())
-    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미 (a -> b 의 비용이 c)
-    edges.append((a, b, c))
+# 최단 거리 값을 모두 무한으로 초기화
+node = {
+    'A': INF,
+    'B': INF,
+    'C': INF,
+    'D': INF,
+    'E': INF,
+    'F': INF,
+    'G': INF,
+}
 
+# 정점의 개수, 변의 개수를 입력
+sides_len = len(sides)
+node_len = len(node)
 
 def bellman_ford(start):
-    # 시작 노드에 대해서 초기화
-    distance[start] = 0
-    # 전체 v - 1번의 라운드(round)를 반복
-    for i in range(v):
-        # 매 반복마다 '모든 간선'을 확인한다.
-        for j in range(e):
-            cur_node = edges[j][0]
-            next_node = edges[j][1]
-            edge_cost = edges[j][2]
-            # 현재 간선을 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
-            if distance[cur_node] != INF and distance[next_node] > distance[cur_node] + edge_cost:
-                distance[next_node] = distance[cur_node] + edge_cost
-                # v번째 라운드에서도 값이 갱신된다면 음수 순환이 존재
-                if i == v - 1:
+    # 시작 정점에 대해서 초기화
+    node[start] = 0
+    # 전체 node_len - 1번을 반복
+    for i in range(node_len):
+        # 매 반복마다 '모든 변'을 확인한다.
+        for j in range(sides_len):
+            cur_node = sides[j][0]
+            next_node = sides[j][1]
+            edge_cost = sides[j][2]
+            # 현재 변을 거쳐서 다른 정점로 이동하는 거리가 더 짧은 경우 갱신
+            if node[cur_node] != INF and node[next_node] > node[cur_node] + edge_cost:
+                node[next_node] = node[cur_node] + edge_cost
+                # node_len번째에도 값이 갱신된다면 음수 순환이 존재
+                if i == node_len - 1:
                     return True
 
     return False
 
 
-# 벨만 포드 알고리즘 수행
-negative_cycle = bellman_ford(1)
+# 벨만 포드 알고리즘 실행
+negative_cycle = bellman_ford('A')
 
 # 음수 순환이 존재하면 -1 출력
 if negative_cycle:
-    print("-1")
+    print("순환이 존재")
 else:
-    # 1번 노드를 제외한 다른 모든 노드로 가기 위한 최단 거리를 출력
-    for i in range(2, v + 1):
+    # 1번 정점을 제외한 다른 모든 정점으로 가기 위한 최단 거리를 출력
+    for key, value in node.items():
         # 도달할 수 없는 경우, -1 출력
-        if distance[i] == INF:
-            print("-1")
+        if value == INF:
+            print(key, '도달 할 수 없음')
         # 도달할 수 있으면 거리 출력
         else:
-            print(distance[i])
+            print(key, value)
