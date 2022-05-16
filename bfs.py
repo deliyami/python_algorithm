dccf5c96bce52706a13b1dca3c5ec780e1cab23f
deliyami <dkdidudtn@gmail.com>
2022-05-16 16:48:54 +0900

    FIX - annotation BFS, DFS

--- bfs.py
+++ bfs.py
@@ -1,6 +1,7 @@
 from collections import deque
 
-graph_list = {'A': set(['B','C','D']),
+# 모든 정점과 해당 정점과 연결된 정점의 정보 입력
+node_list = {'A': set(['B','C','D']),
               'B': set(['E','F']),
               'C': set(['H']),
               'D': set(['I','J']),
@@ -12,22 +13,29 @@ graph_list = {'A': set(['B','C','D']),
               'K': set([]),
               'G': set([]),
               'L': set([]),}
+
+# 시작 정점
 root_node = 'A'
+
+# 도착 정점
 check_node = 'G'
 
 def BFS_with_adj_list(graph, root):
+		# 방문한 정점에 대해서 초기화
     visited = []
     queue = deque([root])
-    find_node = ''
+		# queue에 값이 없을 때 까지 반복
     while queue:
+				# queue에 들어있는 값을 꺼내오고 도착 정점과 같은지 확인
         n = queue.popleft()
         if check_node == n:
-            find_node = n
-            break
+            return n
+				# 현재 정점을 확인
         print('this node:',n)
+				# 현재 정점을 방문 한 적이 없다면 현재 정점에 연결된 정점을 queue에 삽입
         if n not in visited:
             visited.append(n)
             queue += graph[n] - set(visited)
-    return find_node
-  
-print(BFS_with_adj_list(graph_list, root_node))
+    
+# BFS 실행
+print(BFS_with_adj_list(node_list, root_node))
--- dfs.py
+++ dfs.py
@@ -1,32 +1,79 @@
-graph_list = {'A': set(['B','C','D']),
-              'B': set(['E','F']),
-              'C': set(['H']),
-              'D': set(['I','J']),
-              'E': set(['K']),
-              'F': set([]),
-              'H': set(['G']),
-              'I': set([]),
-              'J': set(['L']),
-              'K': set([]),
-              'G': set([]),
-              'L': set([]),}
-root_node = 'A'
-check_node = 'G'
+INF = int(1e9)
 
-def DFS_with_adj_list(graph, root):
-    visited = []
-    stack = [root]
-    find_node = ''
-    while stack:
-        n = stack.pop()
-        if check_node == n:
-            find_node = n
-            break
-        print('this node:',n)
-        if n not in visited:
-            visited.append(n)
-            stack += graph[n] - set(visited)
-    return find_node
+# 모든 변의 정보 입력
+sides = [
+    ['A', 'B', 9],
+	    ['A', 'C', 2],
+    ['B', 'A', 9],
+    ['B', 'C', 6],
+    ['B', 'D', 3],
+    ['B', 'E', 1],
+    ['C', 'A', 2],
+    ['C', 'B', 6],
+    ['C', 'D', 2],
+    ['C', 'F', 9],
+    ['D', 'B', 3],
+    ['D', 'C', 2],
+    ['D', 'E', 5],
+    ['D', 'F', 6],
+    ['E', 'B', 1],
+    ['E', 'D', 5],
+    ['E', 'F', 3],
+    ['E', 'G', 7],
+    ['F', 'C', 9],
+    ['F', 'D', 6],
+    ['F', 'E', 3],
+    ['F', 'G', 4],
+    ['G', 'E', 7],
+    ['G', 'F', 4],
+]
 
-print(DFS_with_adj_list(graph_list, root_node))
+# 최단 거리 값을 모두 무한으로 초기화
+node = {
+    'A': INF,
+    'B': INF,
+    'C': INF,
+    'D': INF,
+    'E': INF,
+    'F': INF,
+    'G': INF,
+}
 
+# 정점의 개수, 변의 개수를 입력
+sides_len = len(sides)
+node_len = len(node)
+
+def bellman_ford(start):
+    # 시작 정점에 대해서 초기화
+    node[start] = 0
+    # 전체 node_len - 1번을 반복
+    for i in range(node_len):
+        # 매 반복마다 '모든 변'을 확인한다.
+        for j in range(sides_len):
+            cur_node = sides[j][0]
+            next_node = sides[j][1]
+            edge_cost = sides[j][2]
+            # 현재 변을 거쳐서 다른 정점로 이동하는 거리가 더 짧은 경우 갱신
+            if node[cur_node] != INF and node[next_node] > node[cur_node] + edge_cost:
+                node[next_node] = node[cur_node] + edge_cost
+                # node_len번째에도 값이 갱신된다면 음수 순환이 존재
+                if i == node_len - 1:
+                    return True
+
+    return False
+
+# 벨만 포드 알고리즘 실행
+negative_cycle = bellman_ford('A')
+
+# 음수 순환이 존재하면 -1 출력
+if negative_cycle:
+    print("순환이 존재")
+else:
+    # 1번 정점을 제외한 다른 모든 정점으로 가기 위한 최단 거리를 출력
+    for key, value in node.items():
+        # 도달할 수 없는 경우, -1 출력
+        if value == INF:
+            print(key, '도달 할 수 없음')
+        # 도달할 수 있으면 거리 출력
+        else:
+            print(key, value)
